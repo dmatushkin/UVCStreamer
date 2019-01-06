99e2509873921b04072609f25dc60137e3ec6ea1
dmatushkin <dmatushkin@gmail.com>
2019-01-06 15:15:05 +0400

    Initial Commit

---
+++ UVCStreamer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UVCStreamer
+//
+//  Created by Dmitry Matyushkin on 06/01/2019.
+//  Copyright © 2019 Dmitry Matyushkin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ UVCStreamer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UVCStreamer
+//
+//  Created by Dmitry Matyushkin on 06/01/2019.
+//  Copyright © 2019 Dmitry Matyushkin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
